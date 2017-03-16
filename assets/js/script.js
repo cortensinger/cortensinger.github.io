e777122ba4ec143c808c08d27bf6b4f7c568c4f9
Corten Singer <cortensinger@Cortens-MacBook-Pro.local>
2017-03-16 02:46:55 -0700

    Progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,11 +38,8 @@ function showMenu() {
     $('#container').addClass('adjust-container');
     setTimeout(function () {
         $('#menu').addClass('show-menu');
-        $('#menu-logo').addClass('show-menu-logo');
     }, 50);
-    setTimeout(function () {
-        $('#menu-line').css({'bottom': '0px'});
-    }, 250);
+
     setTimeout(function () {
         var position = $('#about').position().left,
             width = $('#about').innerWidth();
@@ -51,7 +48,7 @@ function showMenu() {
             'left': position,
             'width': width
         });
-    }, 1000);
+    }, 0);
 }
 
 function cleanProjects() {
@@ -66,36 +63,16 @@ function cleanProjects() {
     $('#container').removeClass('no-scroll');
 }
 
+
 $(document).ready(function () {
     var currentPanel = '#panel-about';
     var currentButton = '#about';
-    $('#plus-cover, #plus').click(function () {
-        $('#plus-cover').css({'display': 'none'});
-        $('#t, #o, #plus').css({'transition': '0.5s', '-webkit-transition': '0.5s'});
-        $('#t, #o').addClass('shrink-title');
-        $('#plus').css({'color': ''});
-        setTimeout(function () {
+    setTimeout(function () {
             showMenu();
-            spinLittlePlus();
-            $('#plus').css({'color': '#FFFEFC'});
         }, 600);
-        setTimeout(function () {
-            $('.letter').fadeOut(500);
-        }, 800);
-        setTimeout(function () {
+    setTimeout(function () {
             showPanels();
         }, 1600);
-        setTimeout(function () {
-            $('#plus').css({
-                'animation-play-state': 'paused',
-                '-webkit-animation-play-state': 'paused'
-            });
-        }, 2000);
-    });
-
-    $('#plus-cover').hover(function () {
-        spinPlus();
-    });
 
     $('.menu-item').click(function () {
         var id = $(this).attr('id'),
@@ -117,52 +94,52 @@ $(document).ready(function () {
         }
     });
 
-    $('#button-download').hover(function () {
-        $(this).addClass('button-effect');
-        $('#button-popup').addClass('button-popup-effect');
-    }, function () {
-        $(this).addClass('button-effect-reverse');
-        $('#button-popup').addClass('button-popup-effect-reverse');
-        $(this).removeClass('button-effect');
-        $('#button-popup').removeClass('button-popup-effect');
-        setTimeout(function () {
-            $(this).removeClass('button-effect-reverse');
-            $('#button-popup').removeClass('button-popup-effect-reverse');
-        }, 1000);
-    });
+//    $('#button-download').hover(function () {
+//        $(this).addClass('button-effect');
+//        $('#button-popup').addClass('button-popup-effect');
+//    }, function () {
+//        $(this).addClass('button-effect-reverse');
+//        $('#button-popup').addClass('button-popup-effect-reverse');
+//        $(this).removeClass('button-effect');
+//        $('#button-popup').removeClass('button-popup-effect');
+//        setTimeout(function () {
+//            $(this).removeClass('button-effect-reverse');
+//            $('#button-popup').removeClass('button-popup-effect-reverse');
+//        }, 1000);
+//    });
     
-    $('.contact-element').click(function () {
-        var links = {
-            'linkedin': 'https://www.linkedin.com/in/vegatomas',
-            'github': 'https://github.com/tomasero',
-            'email': 'mailto:me@tomasvega.com'
-        },
-            dest = $(this).text().trim().toLowerCase();
-        console.log(dest);
-        window.open(links[dest], '_blank');
-    });
+//    $('.contact-element').click(function () {
+//        var links = {
+//            'linkedin': 'https://www.linkedin.com/in/vegatomas',
+//            'github': 'https://github.com/tomasero',
+//            'email': 'mailto:me@tomasvega.com'
+//        },
+//            dest = $(this).text().trim().toLowerCase();
+//        console.log(dest);
+//        window.open(links[dest], '_blank');
+//    });
     
-    $('#button-download').click(function () {
-        var win = window.open('/assets/docs/TomasVega.pdf', '_blank');
-        win.focus();
-    });
+//    $('#button-download').click(function () {
+//        var win = window.open('/assets/docs/TomasVega.pdf', '_blank');
+//        win.focus();
+//    });
     
     $(window).resize(function () {
         setTimeout(function () {
             $('#menu-current').css({
                 'left': $(currentButton).position().left,
                 'width': $(currentButton).innerWidth()
-            });             
+            });
         }, 500);
     });
     
     $('.project-cell').click(function () {
-    var id = $(this).attr('id').split('-')[2],
-        project = "#project-" + id,
-        position = $(this).position(),
-        width = $(this).innerWidth(),
-        height = $(this).innerHeight(),
-        borderRadius = $(this).css('border-radius');
+        var id = $(this).attr('id').split('-')[2],
+            project = "#project-" + id,
+            position = $(this).position(),
+            width = $(this).innerWidth(),
+            height = $(this).innerHeight(),
+            borderRadius = $(this).css('border-radius');
         console.log(id);
         console.log(project);
         console.log(position);
@@ -201,24 +178,24 @@ $(document).ready(function () {
         $('#container').removeClass('no-scroll');
     });
     
-    $('.link-svg').click(function () {
-        var id = $(this).attr('id').split('-')[2];
-        var links = {
-            1: 'https://github.com/lambdaloop/wheelsense',
-            4: 'http://elblog.tomasvega.com',
-            5: 'https://docs.google.com/presentation/d/1I3RU1kvsPxRFHs33R6WiZtBUyDdlfxgRLMnOsz0lfmY/edit?usp=sharing',
-            6: 'https://www.hackster.io/synthsense/synthsense-f62fc1',
-            7: 'https://www.hackster.io/golden-goats/biokneek-74752f',
-            8: 'https://youtu.be/rUajyflnPHQ',         
-            9: 'https://www.hackster.io/team-taboo/my-flow',
-            10: 'https://github.com/tomasero/mind-kinetics',
-            11: 'http://challengepost.com/software/mydrone',
-            12: 'https://github.com/tomasero/haptic-braille'
-            
-            
-        };
-        console.log(parseInt(id));
-        window.open(links[parseInt(id)], '_blank');
-    });
-    
+//    $('.link-svg').click(function () {
+//        var id = $(this).attr('id').split('-')[2];
+//        var links = {
+//            1: 'https://github.com/lambdaloop/wheelsense',
+//            4: 'http://elblog.tomasvega.com',
+//            5: 'https://docs.google.com/presentation/d/1I3RU1kvsPxRFHs33R6WiZtBUyDdlfxgRLMnOsz0lfmY/edit?usp=sharing',
+//            6: 'https://www.hackster.io/synthsense/synthsense-f62fc1',
+//            7: 'https://www.hackster.io/golden-goats/biokneek-74752f',
+//            8: 'https://youtu.be/rUajyflnPHQ',         
+//            9: 'https://www.hackster.io/team-taboo/my-flow',
+//            10: 'https://github.com/tomasero/mind-kinetics',
+//            11: 'http://challengepost.com/software/mydrone',
+//            12: 'https://github.com/tomasero/haptic-braille'
+//            
+//            
+//        };
+//        console.log(parseInt(id));
+//        window.open(links[parseInt(id)], '_blank');
+//    });
+//    
 });
